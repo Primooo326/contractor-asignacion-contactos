e656e4222c707c841457647001f0f3d6a2ff709b
wixpor <jhonjhon921109@gmail.com>
2024-11-14 17:47:53 -0500

    Ajustes endpoint services.leadconnectorhq.com/users/search

--- src/api/goHighLevel/userA.api.ts
+++ src/api/goHighLevel/userA.api.ts
@@ -1,8 +1,8 @@
 // https://services.leadconnectorhq.com/users/search?companyId={{CompanyId}}&query=yesica.mica@contractorlevelup.com&locationId={{LocationIdContractor}}
 
+import { CONTRACTOR_COMPANY_ID, CONTRACTOR_LOCATION_ID } from "@/config";
 import { fetchApiContractor } from "../instances";
 
 export async function getUserAByEmail(email: string): Promise<IUserAReturn> {
-    return fetchApiContractor.get(`users/search?query=${email}`);
+    return fetchApiContractor.get(`users/search?query=${email}&companyId=${CONTRACTOR_COMPANY_ID}&locationId=${CONTRACTOR_LOCATION_ID}`);
 }
-
--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@ export const CONTRACTOR_KEY = process.env.NEXT_PUBLIC_CONTRACTOR_KEY || "";
 export const CONTRACTOR_LOCATION_ID = process.env.NEXT_PUBLIC_CONTRACTOR_LOCATION_ID || "";
 
 export const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET || "";
+
+export const CONTRACTOR_COMPANY_ID = process.env.NEXT_PUBLIC_CONTRACTOR_COMPANY_ID || "";
